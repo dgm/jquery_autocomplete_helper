eefe5484c7d9bb2d274df32854255b92c216eab8
David Morton <mortonda@dgrmm.net>
2009-12-07 20:28:39 -0600

    store id value in hidden attribute too!

--- lib/jquery_autocomplete.rb
+++ lib/jquery_autocomplete.rb
@@ -10,8 +10,8 @@ module ActionView
           InstanceTag.new(object_name, method, self, options.delete(:object)).to_autocomplete_field_tag(options)
        end
     end
-    
-    
+
+
     class InstanceTag
       def to_autocomplete_field_tag(options = {})
         options = options.stringify_keys
@@ -19,7 +19,8 @@ module ActionView
         options = DEFAULT_FIELD_OPTIONS.merge(options)
 
         options["type"] = "text"
-        options["value"] ||= value_before_type_cast(object)
+        options_value = options['value']
+        options["value"] ||= search_value_before_type_cast(object)
         options["value"] &&= html_escape(options["value"])
         options_id = options["id"]  #store for later
         options_name = options["name"] #store for later
@@ -33,11 +34,15 @@ module ActionView
         options.delete("url")
         options["id"] = options_id
         options["name"] = options_name
-        
+        options['value'] = options_value
+        options["value"] ||= value_before_type_cast(object)
+        options["value"] &&= html_escape(options["value"])
+
+
         add_default_name_and_id(options)
         result += tag("input", options)
       end
-      
+
       def to_jquery_autocomplete_script(options = {})
         jcode =<<-EOC
         $("#ac_#{search_tag_id}").autocomplete("#{options[:url]}", {
@@ -50,15 +55,15 @@ module ActionView
         });
         EOC
       end
-      
+
       private
-      
+
       def add_search_name_and_id(options)
           options["name"] = "ac_" + search_tag_name
           options["id"] = "ac_" + search_tag_id
       end
-      
-      
+
+
       def search_tag_name
         "#{@object_name}[#{search_sanitized_method_name}]"
       end
@@ -70,12 +75,16 @@ module ActionView
       def search_sanitized_method_name
         @search_sanitized_method_name ||= @method_name.sub(/\?$/,"").sub(/_id$/,"")
       end
+
+      def search_value_before_type_cast(object)
+        search_value = self.class.value_before_type_cast(object, @method_name.sub(/_id$/,""))
+        search_value ? search_value.display_name : ""
+      end
     end
-    
+
     class FormBuilder
       def autocomplete_field(method, options = {})
         @template.autocomplete_field(@object_name, method, objectify_options(options))
-        
       end
     end
   end
